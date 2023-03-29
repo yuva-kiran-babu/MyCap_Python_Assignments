3f4d65d1b3563e036badaa6dc58665ddaad3f0cc
yuva-kiran-babu <128019807+yuva-kiran-babu@users.noreply.github.com>
2023-03-29 18:09:19 +0530

    Add files via upload

---
+++ Assignment_3.py
@@ -0,0 +1,13 @@
+# program to print all positive numbers in a range
+
+list1=[12,-7,5,64,-14]
+list2=[12,14,-95,3,]
+
+for number in list1:
+    if number>0:
+        print(number)
+
+
+for number in list2:
+    if number>0:
+        print(number)
